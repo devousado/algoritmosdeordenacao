a52be48ee3588077d0057a10760a183a6a155d02
cahoco <cahocofelizardo@gmail.com>
2023-09-16 00:02:37 +0100

    primeiro commit

---
+++ bin/algoritmos_de_ordenacao.dart
@@ -0,0 +1,5 @@
+import 'package:algoritmos_de_ordenacao/algoritmos_de_ordenacao.dart' as algoritmos_de_ordenacao;
+
+void main(List<String> arguments) {
+  print('Hello world: ${algoritmos_de_ordenacao.calculate()}!');
+}
---
+++ test/algoritmos_de_ordenacao_test.dart
@@ -0,0 +1,8 @@
+import 'package:algoritmos_de_ordenacao/algoritmos_de_ordenacao.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
